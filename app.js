96f7c01e969e7d6bf1d2ca9138d5c685744978e2
SebastianStupp <s.stupp@outlook.de>
2020-03-23 18:27:38 +0100

    Basic Node Functions

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-function sayPassword() {
-  console.log("PAsswords");
-}
+const [command, key, value] = process.argv.slice(2);
 
-sayPassword();
+const { get, set, unset } = require('./lib/commands');
+
+if (command === 'get') {
+  get(key);
+} else if (command === 'set') {
+  set(key, value);
+} else if (command === 'unset') {
+  unset(key);
+}
