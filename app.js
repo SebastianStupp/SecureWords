efa5b404fe30aa8a7d617690674c160923540078
SebastianStupp <s.stupp@outlook.de>
2020-03-24 11:37:26 +0100

    Change Function Export Name

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const [command, key] = process.argv.slice(2);
 const { get, set, unset } = require('./lib/commands');
-const { ask } = require('./lib/inputs');
+const { askForPassword } = require('./lib/inputs');
 
 async function run() {
   if (command === 'get') {
     get(key);
   } else if (command === 'set') {
-    const password = await ask(key);
+    const password = await askForPassword(key);
     set(key, password);
   } else if (command === 'unset') {
     unset(key);
