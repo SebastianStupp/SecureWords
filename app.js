6942c6e4380ab20748833d24b0311d6d26bd1177
SebastianStupp <s.stupp@outlook.de>
2020-03-24 11:32:48 +0100

    Add readline Interface

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-const [command, key, value] = process.argv.slice(2);
-
+const [command, key] = process.argv.slice(2);
 const { get, set, unset } = require('./lib/commands');
+const { ask } = require('./lib/inputs');
 
-if (command === 'get') {
-  get(key);
-} else if (command === 'set') {
-  set(key, value);
-} else if (command === 'unset') {
-  unset(key);
+async function run() {
+  if (command === 'get') {
+    get(key);
+  } else if (command === 'set') {
+    const password = await ask(key);
+    set(key, password);
+  } else if (command === 'unset') {
+    unset(key);
+  }
 }
+run();
